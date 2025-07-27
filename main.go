9842e0190fbddaaec063d40021bf9bf11e3a6416
kenya6111 <kenyanke6111@gmail.com>
2025-07-27 21:54:56 +0900

    3-1,3-2

--- main.go
+++ main.go
@@ -1,60 +1,30 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/kenya6111/go-intermediate-api/handlers"
 )
-type Comment struct {
-CommentID int `json:"comment_id"`
-ArticleID int `json:"article_id"`
-Message string `json:"message"`
-CreatedAt time.Time `json:"created_at"`
-}
-type Article struct {
-ID int `json:"article_id"`
-Title string `json:"title"`
-Contents string `json:"contents"`
-UserName string `json:"user_name"`
-NiceNum int `json:"nice"`
-CommentList []Comment `json:"comments"`
-CreatedAt time.Time `json:"created_at"`
-}
-
 func main(){
-	comment1 := Comment{
-		CommentID: 1,
-		ArticleID: 1,
-		Message: "test comment1",
-		CreatedAt: time.Now(),
-	}
-	comment2 := Comment{
-		CommentID: 2,
-		ArticleID: 1,
-		Message: "second comment",
-		CreatedAt: time.Now(),
-	}
-	article := Article{
-		ID: 1,
-		Title: "first article",
-		Contents: "This is the test article.",
-		UserName: "saki",
-		NiceNum: 1,
-		CommentList: []Comment{comment1, comment2},
-		CreatedAt: time.Now(),
-	}
-	jsonData,err := json.Marshal(article)
-	if err != nil{
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "intermediateDB"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
 		fmt.Println(err)
-		return
-
 	}
-	fmt.Printf("%s\n",jsonData)
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/hello",handlers.HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article",handlers.PostArticleHandler).Methods(http.MethodPost)
