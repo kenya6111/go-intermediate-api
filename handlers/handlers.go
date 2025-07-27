96f190b37fc5565f63972fd26e93ec03782bf476
kenya6111 <kenyanke6111@gmail.com>
2025-07-27 14:44:23 +0900

    1-2

---
+++ handlers/handlers.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+
+func HelloHandler (w http.ResponseWriter, req * http.Request){
+	io.WriteString(w, "Hello, wold!\n")
+}
+
+func PostArticleHandler (w http.ResponseWriter, req * http.Request){
+	io.WriteString(w, "post article!\n")
+}
+
+func ArticleListHandler (w http.ResponseWriter, req * http.Request){
+	io.WriteString(w, "get article list!\n")
+}
+
+func ArticleDetailHandler (w http.ResponseWriter, req * http.Request){
+	articleId := 1
+	resString := fmt.Sprintf("ArticleNo.%d\n",articleId)
+	io.WriteString(w, resString)
+}
+
+func PostNiceHandler(w http.ResponseWriter, req * http.Request){
+	io.WriteString(w, "post nice!\n")
+}
+
+func PostCommentHandler(w http.ResponseWriter, req * http.Request){
+	io.WriteString(w, "post comment!\n")
+}
--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/kenya6111/go-intermediate-api/handlers"
 )
 
 func main(){
-	helloHandler := func(w http.ResponseWriter, req * http.Request){
-		io.WriteString(w, "Hello, wold!\n")
-	}
 
-	http.HandleFunc("/",helloHandler)
+	http.HandleFunc("/",handlers.HelloHandler)
+	http.HandleFunc("/article",handlers.PostArticleHandler)
+	http.HandleFunc("/article/list",handlers.ArticleListHandler)
+	http.HandleFunc("/article/1",handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice",handlers.PostNiceHandler)
+	http.HandleFunc("/comment",handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	fmt.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
