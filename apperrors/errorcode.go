22eab979a525e92f5bcb7501ff1213f8ea747423
kenya6111 <kenyanke6111@gmail.com>
2025-08-06 21:48:09 +0900

    7-2

--- apperrors/errorcode.go
+++ apperrors/errorcode.go
@@ -3,5 +3,10 @@ package apperrors
 type ErrCode string
 
 const (
-	Unknown ErrCode = "U000" //開発者が想定していないエラーが発生した」ときに使うことを想定したエラーコード
+	Unknown          ErrCode = "U000" //開発者が想定していないエラーが発生した」ときに使うことを想定したエラーコード
+	InsertDataFailed ErrCode = "S001"
+	GetDataFailed    ErrCode = "S002"
+	NAData           ErrCode = "S003"
+	NoTargetData     ErrCode = "S004"
+	UpdateDataFailed ErrCode = "S005"
 )
--- services/comment_service.go
+++ services/comment_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/kenya6111/go-intermediate-api/apperrors"
 	"github.com/kenya6111/go-intermediate-api/models"
 	"github.com/kenya6111/go-intermediate-api/repositories"
 )
@@ -10,6 +11,7 @@ import (
 func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Comment{}, err
 	}
 
