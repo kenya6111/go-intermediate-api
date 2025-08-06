5441a6d9e3d953d750318f18020bbd8c110c4160
kenya6111 <kenyanke6111@gmail.com>
2025-08-06 21:56:58 +0900

    7-3

--- apperrors/errorcode.go
+++ apperrors/errorcode.go
@@ -9,4 +9,7 @@ const (
 	NAData           ErrCode = "S003"
 	NoTargetData     ErrCode = "S004"
 	UpdateDataFailed ErrCode = "S005"
+
+	ReqBodyDecodeFailed ErrCode = "R001"
+	BadParam            ErrCode = "R002"
 )
--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/kenya6111/go-intermediate-api/apperrors"
 	"github.com/kenya6111/go-intermediate-api/controllers/services"
 	"github.com/kenya6111/go-intermediate-api/models"
 )
@@ -29,6 +30,7 @@ func (c *ArticleController) HelloHandler(w http.ResponseWriter, req *http.Reques
 func (c *ArticleController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		err = apperrors.ReqBodyDecodeFailed.Wrap(err, "bad request body")
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
@@ -48,6 +50,7 @@ func (c *ArticleController) ArticleListHandler(w http.ResponseWriter, req *http.
 		var err error
 		page, err = strconv.Atoi(p[0])
 		if err != nil {
+			err = apperrors.BadParam.Wrap(err, "queryparam must be number")
 			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 			return
 		}
@@ -66,6 +69,7 @@ func (c *ArticleController) ArticleListHandler(w http.ResponseWriter, req *http.
 func (c *ArticleController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
+		err = apperrors.BadParam.Wrap(err, "pathparam must be number")
 		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 		return
 	}
@@ -85,6 +89,7 @@ func (c *ArticleController) PostNiceHandler(w http.ResponseWriter, req *http.Req
 
 	article, err := c.service.PostNiceService(reqArticle)
 	if err != nil {
+		err = apperrors.ReqBodyDecodeFailed.Wrap(err, "bad request body")
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
 	}
